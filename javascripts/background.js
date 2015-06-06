c623e3e6b42ee0986f4713f2aeed511b5c026b22
unFearing <chen240sx@gmail.com>
2015-06-06 13:18:01 -0500

    sssss

--- javascripts/background.js
+++ javascripts/background.js
@@ -21,3 +21,34 @@ $('.table-down').click(function () {
   var $row = $(this).parents('tr');
   $row.next().after($row.get(0));
 });
+
+// A few jQuery helpers for exporting only
+jQuery.fn.pop = [].pop;
+jQuery.fn.shift = [].shift;
+
+$BTN.click(function () {
+  var $rows = $TABLE.find('tr:not(:hidden)');
+  var headers = [];
+  var data = [];
+  
+  // Get the headers (add special header logic here)
+  $($rows.shift()).find('th:not(:empty)').each(function () {
+    headers.push($(this).text().toLowerCase());
+  });
+  
+  // Turn all existing rows into a loopable array
+  $rows.each(function () {
+    var $td = $(this).find('td');
+    var h = {};
+    
+    // Use the headers from earlier to name our hash keys
+    headers.forEach(function (header, i) {
+      h[header] = $td.eq(i).text();   
+    });
+    
+    data.push(h);
+  });
+  
+  // Output the result
+  $EXPORT.text(JSON.stringify(data));
+});
