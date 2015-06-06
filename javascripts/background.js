aabc6228a52b29c7ecaf16e31efde0245c42775f
unFearing <chen240sx@gmail.com>
2015-06-06 17:51:30 -0500

    test2

--- javascripts/background.js
+++ javascripts/background.js
@@ -1,6 +1,4 @@
 var $TABLE = $('#table');
-var $BTN = $('#export-btn');
-var $EXPORT = $('#export');
 
 $('.table-add').click(function () {
   var $clone = $TABLE.find('tr.hide').clone(true).removeClass('hide table-line');
@@ -9,15 +7,4 @@ $('.table-add').click(function () {
 
 $('.table-remove').click(function () {
   $(this).parents('tr').detach();
-});
-
-$('.table-up').click(function () {
-  var $row = $(this).parents('tr');
-  if ($row.index() === 1) return; // Don't go above the header
-  $row.prev().before($row.get(0));
-});
-
-$('.table-down').click(function () {
-  var $row = $(this).parents('tr');
-  $row.next().after($row.get(0));
 });
