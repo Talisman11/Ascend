583310206036fd7b86ff1ce838e3dfee9772622b
unFearing <chen240sx@gmail.com>
2015-06-06 21:06:58 -0500

    adsasdasdsaddsadsa

--- javascripts/background.js
+++ javascripts/background.js
@@ -10,7 +10,7 @@ $('.table-remove').click(function () {
 });
 
 function insert() {
-    var table = document.getElementById("data-table");
+    var table = document.getElementById("table");
     var row = table.insertRow(0);
     var cell1 = row.insertCell(0);
     var cell2 = row.insertCell(1);
